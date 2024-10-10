43317370029d22802a3057a47e55041280aee343
AjabaGodwill <ajabagodswill@gmail.com>
2024-10-10 23:54:30 +0100

    Added posts edit route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -20,6 +20,16 @@ router.post("/api/createPost", (req, res, next) => {
     } )
 })
 
+router.put("/api/editPost", (req, res, next) => {
+    const value = [req.body.title, req.body.body, req.body.uid, req.body.pid, req.body.username]
+    pool.query(`UPDATE posts SET titl=$1, body=$2, userId=$3, author=$5, date_created= NOW()
+    WHERE pid=$4` , value, (qErr, qRes) => {
+        if (qErr) return next(qErr)
+        res.json(qRes.rows)
+    })
+
+})
+
 
 
 
