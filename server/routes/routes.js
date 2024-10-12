214b3bf66723437265c0c01541e88fc38afdd504
AjabaGodwill <ajabagodswill@gmail.com>
2024-10-12 07:56:12 +0100

    Added routes for CRUD operation on comments

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -16,7 +16,8 @@ router.post("/api/createPost", (req, res, next) => {
     VALUES($1,$2,$3,$4 NOW())`, value, (qErr, qRes) => {
         if(qErr) return next(qErr)
         res.json(qRes.rows)
-    } )
+        console.log(qErr)
+    })
 })
 
 router.put("/api/editPost", (req, res, next) => {
@@ -47,6 +48,50 @@ router.delete("/api/deletePost", (req, res, next) => {
         res.json(qRes.rows)
         console.log(qErr)
     })
+})
+
+// COMMENTS ROUTE
+router.get("/api/allPostComment", (req, res, next) => {
+    const post_id = String(req.query.post_id)
+    pool.query("SELECT * FROM comments WHERE post_id=$1", [post_id],
+        (qErr, qRes) => {
+            res.json(qRes.rows)
+            console.log(qErr)
+
+    })
+
+})
+router.post("/api/createComment", (req, res, next) => {
+    const values = [req.body.comment, req.body.user_id, req.body.username, req.body.post_id]
+    pool.query("INSERT INTO comments(comment, user_id, author, post_id, date_created) VALUES($1,$2,$3,$4 NOW())",
+        values, (qErr, qRes) => {
+            res.json(qRes.rows)
+            console.log(qErr)
+
+    })
+
+})
+
+router.put("/api/editcomment", (req, res, next) => {
+    const values = [req.body.comment, req.body.user_id, req.body.post_id,req.body.username, req.body.cid]
+    pool.query("UPDATE comments SET (comment = $1, user_id =$2, post_id=$3 author =$4, date_created = NOW())) WHERE cid=$5",
+        values, (qErr, qRes) => {
+            res.json(qRes.rows)
+            console.log(qErr)
+
+    })
+
+})
+
+router.delete("/api/deleteComment", (req, res, next) => {
+    const cid = req.body.cid
+    pool.query("DELETE FROM comments WHERE cid=$1", [cid],
+         (qErr, qRes) => {
+            res.json(qRes.rows)
+            console.log(qErr)
+
+    })
+
 })
 
 
